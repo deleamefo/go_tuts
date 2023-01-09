823887fa6390ec8d28421ec24d455566c26195a7
Dele Amefo <creditamefo@gmail.com>
2023-01-09 19:45:27 +0100

    for in go

--- hello.go
+++ hello.go
@@ -4,12 +4,25 @@ import "fmt"
 
 func main() {
 
-	fmt.Println("go" + "lang")
+    i := 1
+    for i <= 3 {
+        fmt.Println(i)
+        i = i + 1
+    }
 
-	fmt.Println("1+1 =", 1+1)
-	fmt.Println("7.0/3.0 =", 7.0/3.0)
+    for j := 7; j <= 9; j++ {
+        fmt.Println(j)
+    }
 
-	fmt.Println(true && false)
-	fmt.Println(true || false)
-	fmt.Println(!true)
+    for {
+        fmt.Println("loop")
+        break
+    }
+
+    for n := 0; n <= 5; n++ {
+        if n%2 == 0 {
+            continue
+        }
+        fmt.Println(n)
+    }
 }
