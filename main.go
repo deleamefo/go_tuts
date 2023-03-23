f579e16d6f771a134ca392f122f2657d6409ff67
Dele Amefo <creditamefo@gmail.com>
2023-03-23 17:22:45 +0100

    types in go

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	roomNumber := 154
+	floorNumber := 3
+
+	fmt.Println(roomNumber, floorNumber)
+
+	const version = 3.142
+	password := "notSecured"
+	fmt.Println(password)
+
+}
